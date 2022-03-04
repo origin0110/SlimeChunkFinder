bb3ae528a977bc3e4b5fe736211f6b746dc85cd0
linlin <52383012+origin0110@users.noreply.github.com>
2022-03-04 22:59:13 +0800

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-slime : slimechunkfinder.c slimechunkfinder.h test.c
-	gcc -o slime -Wall -fopt-info -Ofast -flto -pipe -fopenmp -march=native -mtune=native -ffast-math -finline-functions -finline-limit=99999999 slimechunkfinder.c test.c
+slime : slimechunkfinder.c
+	gcc -o slime -Wall -fexec-charset=GBK -fopt-info -Ofast -flto -pipe -fopenmp -march=native -mtune=native -ffast-math -finline-functions -finline-limit=99999999 slimechunkfinder.c
 
-release : slimechunkfinder.c slimechunkfinder.h test.c
-	gcc -o slime -Wall -s -static -Ofast -flto -pipe -fopenmp -march=native -mtune=native -ffast-math -finline-functions -finline-limit=99999999 slimechunkfinder.c test.c
+release : slimechunkfinder.c
+	gcc -o slime -Wall -fexec-charset=GBK -s -static -Ofast -flto -pipe -fopenmp -march=native -mtune=native -ffast-math -finline-functions -finline-limit=99999999 slimechunkfinder.c
 
 clean :
 	rm slime.exe
--- slimechunkfinder.c
+++ slimechunkfinder.c
@@ -1,42 +1,38 @@
-#include "slimechunkfinder.h"
+#include <math.h>
+#include <omp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-fast_t lut[LUT_LEN] = {0};
+#define LUT_NAME "slimechunk.bin"
+#define LUT_SIZE 0x20000000
 
-fast_t is_slime_chunk_lut(uint32_t s) {
-    return (lut[s / FAST_BITS] & ((1 << (FAST_BITS - 1)) >> (s % FAST_BITS))) != 0;
-}
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-fast_t is_slime_chunk_mt(uint32_t s) {
-    uint32_t m = 0x6c078965 * (s ^ s >> 30) + 1;
-    s = (s & 0x80000000) | (m & 0x7fffffff);
-    for (int i = 2; i < 398; i++)
-        m = 0x6c078965 * (m ^ m >> 30) + i;
-    m ^= (s >> 1) ^ ((-((int32_t)(s & 1))) & 0x9908b0df);
-    m ^= m >> 11;
-    m ^= m << 07 & 0x9d2c5680;
-    m ^= m << 15 & 0xefc60000;
-    m ^= m >> 18;
-    return (fast_t)(!(m % 10));
-}
+static uint8_t lut[LUT_SIZE];
 
 void generate_lut(void) {
-#pragma omp parallel for schedule(static)
-    for (uint32_t i = 0; i < LUT_LEN; i++) {
-        uint32_t seed = i * FAST_BITS;
-        uint32_t s[FAST_BITS];
-        uint32_t m[FAST_BITS];
+#pragma omp parallel for
+    for (uint32_t i = 0; i < LUT_SIZE; i++) {
+        uint32_t seed_0 = i * 8;
+        uint32_t s[8];
+        uint32_t m[8];
+
 #pragma omp simd
-        for (uint32_t j = 0; j < FAST_BITS; j++) {
-            s[j] = seed | j;
+        for (uint32_t j = 0; j < 8; j++) {
+            s[j] = seed_0 | j;
             m[j] = 0x6c078965 * (s[j] ^ s[j] >> 30) + 1;
             s[j] = (s[j] & 0x80000000) | (m[j] & 0x7fffffff);
         }
+
         for (int i = 2; i < 398; i++)
 #pragma omp simd
-            for (uint32_t j = 0; j < FAST_BITS; j++)
+            for (uint32_t j = 0; j < 8; j++)
                 m[j] = 0x6c078965 * (m[j] ^ m[j] >> 30) + i;
+
 #pragma omp simd
-        for (uint32_t j = 0; j < FAST_BITS; j++) {
+        for (uint32_t j = 0; j < 8; j++) {
             m[j] ^= (s[j] >> 1) ^ ((-((int32_t)(s[j] & 1))) & 0x9908b0df);
             m[j] ^= m[j] >> 11;
             m[j] ^= m[j] << 07 & 0x9d2c5680;
@@ -44,119 +40,170 @@ void generate_lut(void) {
             m[j] ^= m[j] >> 18;
             s[j] = (!(m[j] % 10));
         }
-        fast_t tmp = 0;
-        for (uint32_t j = 0; j < FAST_BITS; j++) {
+
+        uint8_t tmp = 0;
+        for (uint32_t j = 0; j < 8; j++) {
             tmp <<= 1;
-            tmp |= (fast_t)s[j];
+            tmp |= (uint8_t)s[j];
         }
+
         lut[i] = tmp;
     }
 }
 
-uint32_t get_seed(int32_t x, int32_t z) {
-    return x * 0x1f1f1f1f ^ z;
+void slime_finder(int XMIN, int ZMIN, int XMAX, int ZMAX, int N, int THR) {
+    const int DZ = ZMAX - ZMIN;
+
+    uint8_t* f2 = calloc(N * DZ, 8);
+    uint8_t* f1 = calloc(DZ, 8);
+
+    uint32_t* tmp_u32 = calloc(DZ, sizeof(uint32_t));
+    int8_t* tmp_i8 = calloc(DZ, sizeof(int8_t));
+
+    for (int32_t x = XMIN; x < XMAX; x++) {
+        size_t xmodN = (x - XMIN) % N;
+        size_t f2_base_index = xmodN * DZ;
+
+#pragma omp simd
+        for (size_t zi = 0; zi < DZ; zi++) {
+            tmp_u32[zi] = ((uint32_t)x * 0x1f1f1f1f) ^ (uint32_t)(zi + ZMIN);
+            tmp_i8[zi] = (1 << (8 - 1)) >> (tmp_u32[zi] % 8);
+            tmp_u32[zi] = tmp_u32[zi] / 8;
+            f1[zi] -= f2[f2_base_index + zi];
+        }
+
+        for (size_t zi = 0; zi < DZ; zi++)  // 没法向量化
+            f2[f2_base_index + zi] = lut[tmp_u32[zi]];
+
+#pragma omp simd
+        for (size_t zi = 0; zi < DZ; zi++) {
+            f2[f2_base_index + zi] = (f2[f2_base_index + zi] & tmp_i8[zi]) != 0;
+            f1[zi] += f2[f2_base_index + zi];
+        }
+
+        for (size_t zi = 0; zi < N; zi++)
+            tmp_i8[zi] = f1[zi];
+#pragma omp simd
+        for (size_t zi = N; zi < DZ; zi++)
+            tmp_i8[zi] = f1[zi] - f1[zi - N];
+
+        int32_t count = 0;
+        for (size_t zi = 0; zi < DZ; zi++) {
+            count += tmp_i8[zi];
+            if (count >= THR) {
+                int32_t z = zi + ZMIN;
+                printf("x=%d,z=%d,n=%d\n", (x + 1) * 16 - (N * 8), (z + 1) * 16 - (N * 8), count);
+            }
+        }
+    }
+
+    free(f2);
+    free(f1);
+    free(tmp_u32);
+    free(tmp_i8);
 }
 
-void swap(int* a, int* b) {
-    if (*a > *b) {
-        int tmp = *a;
-        *a = *b;
-        *b = tmp;
-    }
-}
+int func_f(int x1, int z1, int x2, int z2, int N, int THR) {
+    double time0 = omp_get_wtime();
 
-int slime_initialization(void) {
-    FILE* fp;
-    fp = fopen(LUT_NAME, "rb");
-    if (fp != NULL) {
-        fread(lut, FAST_SIZE, LUT_LEN, fp);
-        fclose(fp);
-        printf("%s has been loaded.\n", LUT_NAME);
-        return 0;
+    const int XMIN = (int)floor(min(x1, x2) / 16.0);
+    const int XMAX = (int)floor(max(x1, x2) / 16.0) + 1;
+    const int ZMIN = (int)floor(min(z1, z2) / 16.0);
+    const int ZMAX = (int)floor(max(z1, z2) / 16.0) + 1;
+    const int DX = XMAX - XMIN;
+    const int DZ = ZMAX - ZMIN;
+
+    if (N < 1) {
+        printf("搜索到一份炒饭\n");
+        return 5;
     }
-    printf("%s no found. Please wait for regenerate.\n", LUT_NAME);
-    generate_lut();
-    fp = fopen(LUT_NAME, "wb");
-    if (fp == NULL) {
-        printf("ERROR: Can not write %s\n", LUT_NAME);
+    if (THR < 1) {
+        printf("搜索到一杯啤酒\n");
+        return 6;
+    }
+    if (DX < N || DZ < N) {
+        printf("搜索到一个笨比\n");
         return 1;
     }
-    fwrite(lut, FAST_SIZE, LUT_LEN, fp);
-    fclose(fp);
-    printf("%s has been regenerate.\n", LUT_NAME);
+    if (DX * DZ < THR) {
+        printf("搜索到一颗桃子\n");
+        return -1;
+    }
+
+    if (DX <= 256 || DZ <= 256) {
+        slime_finder(XMIN, ZMIN, XMAX, ZMAX, N, THR);
+    } else
+#pragma omp parallel
+    {
+        const int thread_num = omp_get_num_threads();
+
+        int z_size = DZ / thread_num;
+
+        int thread_id = omp_get_thread_num();
+        int zmin = ZMIN + thread_id * z_size;
+        int zmax;
+        if (thread_id != thread_num - 1)
+            zmax = ZMIN + (thread_id + 1) * z_size + N - 1;
+        else
+            zmax = ZMAX;
+        slime_finder(XMIN, zmin, XMAX, zmax, N, THR);
+    }
+
+    double time1 = omp_get_wtime();
+    double time = time1 - time0;
+    fprintf(stderr, "搜索结束，用时%lfs，%lfchunks/s\n", time, (uint64_t)DZ * (uint64_t)DX / time);
+
     return 0;
 }
 
-void slime_map(int x1, int z1, int x2, int z2) {
-    swap(&x1, &x2);
-    swap(&z1, &z2);
-
-    for (int z = z1; z <= z2; ++z) {
-        for (int x = x1; x <= x2; ++x) {
-            putchar(is_slime_chunk_lut(get_seed(x, z)) ? '#' : ' ');
-            putchar(' ');
-        }
-        putchar('\n');
+void slime_initialization(void) {
+    FILE* fp = fopen(LUT_NAME, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "找不到重要文件%s，正在尝试重新生成，请耐心等待约一分钟\n", LUT_NAME);
+        generate_lut();
+        fp = fopen(LUT_NAME, "wb");
+        fwrite(lut, 1, LUT_SIZE, fp);
+        fclose(fp);
+        fprintf(stderr, "文件%s已经重新生成，初始化成功\n", LUT_NAME);
+    } else {
+        fread(lut, 1, LUT_SIZE, fp);
+        fclose(fp);
+        fprintf(stderr, "找到文件%s，已加载文件，初始化成功\n", LUT_NAME);
     }
 }
 
-void slime_finder(int x1, int z1, int x2, int z2, int n, int thr) {
-    swap(&x1, &x2);
-    swap(&z1, &z2);
-    int dx = x2 - x1 + 1;
-    int dz = z2 - z1 + 1;
+#define HELP_DOC \
+    "输入命令进行查询，命令列表:\n\
+f x1 z1 x2 z2 n k\t在(x1,z1)到(x2,z2)的坐标范围内，检查所有n*n正方形范围，当史莱姆区块数>=k时输出\n\
+q\t\t\t退出程序，直接点关闭也可以的\n\
+h\t\t\t显示帮助文档，就是就你正在看的这个\n\
+示例: f -10000 -10000 10000 10000 6 14\n"
 
-    double t0, t1;
-    t0 = omp_get_wtime();
+#define WRONG_COMMAND_DOC "格式错误，请输入正确的命令\n"
 
-#pragma omp parallel
-    {
-        int thread_num = omp_get_num_threads();
-        int thread_id = omp_get_thread_num();
-        int x_w = dx / thread_num + ((dx % thread_num) != 0);
-        int x_1 = x1 + thread_id * x_w;
-        int x_2 = x_1 + x_w - 1;
-        x_2 = (x_2 > x2 ? x2 : x_2) + n;
-        printf("thread %d : from %d to %d.\n", thread_id, x_1, x_2 - n);
+int main(void) {
+    slime_initialization();
 
-#pragma omp barrier
+    fprintf(stderr, "输入命令进行查询，输入h并回车获取帮助\n");
 
-        uint8_t* f1 = (uint8_t*)calloc(dz, 1);
-        uint8_t* f2 = (uint8_t*)calloc(n * dz, 1);
-#pragma omp simd
-        for (int x = 0; x < (x_2 - x_1); x++) {
-            int m = x % n;
-            int num = 0;
+    int x1, x2, z1, z2, n, thr, tmp;
 
-            int xpos = x + x_1;
-            uint32_t tmp_get_seed = xpos * 0x1f1f1f1f;
-            uint32_t tmp_a = m * dz;
-#pragma omp simd
-            for (int z = 0; z < n; z++)
-                num += f1[z];
-#pragma omp simd
-            for (int z = n; z < dz; z++) {
-                int zpos = z + z1;
-                uint32_t seed = tmp_get_seed ^ zpos;
-
-                int a = tmp_a + z;
-                f1[z] -= f2[a];
-                f2[a] = is_slime_chunk_lut(seed);
-                f1[z] += f2[a];
-
-                num -= f1[z - n];
-                num += f1[z];
-
-                if (num >= thr)
-                    printf("x=%d,z=%d,n=%d\n", xpos * 16, zpos * 16, num);
-            }
+    while (1) {
+        switch (getchar()) {
+            case 'f':
+                tmp = scanf("%d %d %d %d %d %d", &x1, &x2, &z1, &z2, &n, &thr);
+                if (tmp != 6) {
+                    fprintf(stderr, WRONG_COMMAND_DOC);
+                    break;
+                } else {
+                    func_f(x1, x2, z1, z2, n, thr);
+                }
+                break;
+            case 'h':
+                fprintf(stderr, HELP_DOC);
+                break;
+            case 'q':
+                return 0;
         }
-        free(f1);
-        free(f2);
     }
-    t1 = omp_get_wtime();
-
-    double time = t1 - t0;
-    uint64_t speed = ((uint64_t)dx * (uint64_t)dz) / time;
-    printf("finish in %.3lfs, %llu chunks/s\n", time, speed);
 }
